27220d3c528e0fc9ea99a6f4281f1fb2d091ee07
AjiStone23 <panagiotisbrts@gmail.com>
2022-05-14 12:53:04 +0300

    removing surefire-plugin

--- comment-app/script.sh
+++ comment-app/script.sh
@@ -1,18 +1,13 @@
 
-echo -e "MAVEN"
-mvn clean install
-
-echo -e "EUREKA - SERVER"
-java -jar eureka-server/target/eureka-server-1.0-SNAPSHOT.jar
-
-echo -e "APIGW"
-java -jar apigw/target/apigw-1.0-SNAPSHOT.jar
-
-echo -e "COMMENT - SERVICE"
-java -jar comment-service/target/comment-service-1.0-SNAPSHOT.jar
-echo -e "DASHBOARD - SERVICE"
-java -jar dashboard-service/target/dashboard-service-1.0-SNAPSHOT.jar
-echo -e "COMMENT - SERVICE 2"
-java -Dserver.port=8085 -jar comment-service/target/comment-service-1.0-SNAPSHOT.jar
-echo -e "DASHBOARD - SERVICE 2"
-java -jar -Dserver.port=8086 -Drabbitmq.queues.dashboard=comment.queue2  dashboard-service/target/dashboard-service-1.0-SNAPSHOT.jar
+sleep 1
+gnome-terminal --tab  -t "EUREKA - SERVER" -- bash -c "java -jar eureka-server/target/eureka-server-1.0-SNAPSHOT.jar ; "
+sleep 5
+gnome-terminal --tab  -t "APIGW" -- bash -c "java -jar apigw/target/apigw-1.0-SNAPSHOT.jar ; "
+sleep 5
+gnome-terminal --tab  -t "COMMENT - SERVICE" -- bash -c "java -jar comment-service/target/comment-service-1.0-SNAPSHOT.jar ; "
+sleep 7
+gnome-terminal --tab  -t "DASHBOARD - SERVICE" -- bash -c "java -jar dashboard-service/target/dashboard-service-1.0-SNAPSHOT.jar ; "
+sleep 7
+gnome-terminal --tab  -t "COMMENT - SERVICE 2" -- bash -c "java -Dserver.port=8085 -jar comment-service/target/comment-service-1.0-SNAPSHOT.jar ; "
+sleep 7
+gnome-terminal --tab  -t "DASHBOARD - SERVICE 2" -- bash -c "java -jar -Dserver.port=8086 -Drabbitmq.queues.dashboard=comment.queue2  dashboard-service/target/dashboard-service-1.0-SNAPSHOT.jar ; "
